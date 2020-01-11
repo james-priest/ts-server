de82a64a126171ab7edbdfcf5744a6642d796437
James Priest <james.priest@everbridge.com>
2020-01-10 18:24:10 -0800

    feat: Add login, logout, & protected routes

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not Permitted');
 }
 
 const router = Router();
@@ -33,6 +43,35 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   } else {
     res.send('<div>Invalid email or password</div>');
   }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send(`
+    <div>Welcome to protected route logged in user</div>
+  `);
 });
 
 export { router };
